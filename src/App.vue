25e3cd4118b3550f1a50c1a03389e398459e1099
Satyabrat Jha <satyabrat.jha@vidyamantra.com>
2022-12-06 09:26:50 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MyModal from './components/MyModal.vue'
+</script>
+
+<template>
+  <div class="outer">
+  <h3>Vue Teleport Example</h3>
+  <div>
+    <MyModal />
+  </div>
+</div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/MyModal.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+
+</script>
+
+<template>
+    <button @click="(open = !open)">Open Modal</button>
+
+    <Teleport to="body">
+        <div v-if="open" class="modal">
+            <p>Hello</p>
+            <button @click="(open = !open)">Close</button>
+        </div>
+    </Teleport>
+</template>
+
+<style scoped>
+.modal {
+    position: fixed;
+    z-index: 999;
+    top: 20%;
+    left: 50%;
+    width: 300px;
+    margin-left: -150px;
+    padding: 20px 30px;
+    border-radius: 5px;
+    background: #ddd;
+}
+</style>
